d83e912fc3ece8cb35cff3991bc1d36441bf2faf
Andrey Kurilov <andrey.kurilov@emc.com>
2018-01-25 16:22:00 +0300

    v1.4.3

--- src/main/java/com/github/akurilov/commons/func/Function2.java
+++ src/main/java/com/github/akurilov/commons/func/Function2.java
@@ -7,7 +7,11 @@ public interface Function2<A, B, Z> {
 
 	Z apply(final A a, final B b);
 
-	static <A, B, Z> Function<B, Z> partial(final Function2<A, B, Z> f2, final A a) {
+	static <A, B, Z> Function<B, Z> partial1(final Function2<A, B, Z> f2, final A a) {
 		return (b) -> f2.apply(a, b);
+	}
+
+	static <A, B, Z> Function<A, Z> partial2(final Function2<A, B, Z> f2, final B b) {
+		return (a) -> f2.apply(a, b);
 	}
 }
