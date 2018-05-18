714dc1eb8715b06e48e4db7fd5c47a3a72b39110
Andrey Kurilov <andrey.kurilov@emc.com>
2018-05-18 13:53:01 +0300

    v2.1.1

--- src/main/java/com/github/akurilov/commons/reflection/TypeUtil.java
+++ src/main/java/com/github/akurilov/commons/reflection/TypeUtil.java
@@ -50,32 +50,34 @@ public interface TypeUtil {
 		}
 
 		if(val instanceof Byte) {
+			final Byte b = (Byte) val;
 			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
 				throw new ClassCastException("Cannot cast byte to boolean");
 			}
 			if(cls.equals(char.class) || cls.equals(Character.class)) {
-				return (T) (Object) (char) (((Byte) val).byteValue() & 0xFF);
+				return (T) (Object) (char) (b.byteValue() & 0xFF);
 			}
 			if(cls.equals(short.class) || cls.equals(Short.class)) {
-				return (T) (Object) ((Byte) val).shortValue();
+				return (T) (Object) b.shortValue();
 			}
 			if(cls.equals(int.class) || cls.equals(Integer.class)) {
-				return (T) (Object) ((Byte) val).intValue();
+				return (T) (Object) b.intValue();
 			}
 			if(cls.equals(long.class) || cls.equals(Long.class)) {
-				return (T) (Object) ((Byte) val).longValue();
+				return (T) (Object) b.longValue();
 			}
 			if(cls.equals(float.class) || cls.equals(Float.class)) {
-				return (T) (Object) ((Byte) val).floatValue();
+				return (T) (Object) b.floatValue();
 			}
 			if(cls.equals(double.class) || cls.equals(Double.class)) {
-				return (T) (Object) ((Byte) val).doubleValue();
+				return (T) (Object) b.doubleValue();
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
 			}
 		}
 		if(val instanceof Short) {
+			final Short v = (Short) val;
 			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
 				throw new ClassCastException("Cannot cast short to boolean");
 			}
@@ -83,25 +85,26 @@ public interface TypeUtil {
 				throw new ClassCastException("Cannot cast short to byte");
 			}
 			if(cls.equals(char.class) || cls.equals(Character.class)) {
-				return (T) (Object) (char) (((Short) val).shortValue() & 0xFFFF);
+				return (T) (Object) (char) (v.shortValue() & 0xFFFF);
 			}
 			if(cls.equals(int.class) || cls.equals(Integer.class)) {
-				return (T) (Object) ((Short) val).intValue();
+				return (T) (Object) v.intValue();
 			}
 			if(cls.equals(long.class) || cls.equals(Long.class)) {
-				return (T) (Object) ((Short) val).longValue();
+				return (T) (Object) v.longValue();
 			}
 			if(cls.equals(float.class) || cls.equals(Float.class)) {
-				return (T) (Object) ((Short) val).floatValue();
+				return (T) (Object) v.floatValue();
 			}
 			if(cls.equals(double.class) || cls.equals(Double.class)) {
-				return (T) (Object) ((Short) val).doubleValue();
+				return (T) (Object) v.doubleValue();
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
 			}
 		}
 		if(val instanceof Character) {
+			final char c = (char) val;
 			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
 				throw new ClassCastException("Cannot cast char to boolean");
 			}
@@ -112,22 +115,23 @@ public interface TypeUtil {
 				throw new ClassCastException("Cannot cast char to short");
 			}
 			if(cls.equals(int.class) || cls.equals(Integer.class)) {
-				return (T) (Object) (int) ((char) val);
+				return (T) (Object) (int) c;
 			}
 			if(cls.equals(long.class) || cls.equals(Long.class)) {
-				return (T) (Object) (long) ((char) val);
+				return (T) (Object) (long) c;
 			}
 			if(cls.equals(float.class) || cls.equals(Float.class)) {
-				return (T) (Object) (float) ((char) val);
+				return (T) (Object) (float) c;
 			}
 			if(cls.equals(double.class) || cls.equals(Double.class)) {
-				return (T) (Object) (double) ((char) val);
+				return (T) (Object) (double) c;
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
 			}
 		}
 		if(val instanceof Integer) {
+			final Integer i = (Integer) val;
 			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
 				throw new ClassCastException("Cannot cast int to boolean");
 			}
@@ -141,19 +145,20 @@ public interface TypeUtil {
 				throw new ClassCastException("Cannot cast int to char");
 			}
 			if(cls.equals(long.class) || cls.equals(Long.class)) {
-				return (T) (Object) ((Integer) val).longValue();
+				return (T) (Object) i.longValue();
 			}
 			if(cls.equals(float.class) || cls.equals(Float.class)) {
-				return (T) (Object) ((Integer) val).floatValue();
+				return (T) (Object) i.floatValue();
 			}
 			if(cls.equals(double.class) || cls.equals(Double.class)) {
-				return (T) (Object) ((Integer) val).doubleValue();
+				return (T) (Object) i.doubleValue();
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
 			}
 		}
 		if(val instanceof Long) {
+			final Long v = (Long) val;
 			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
 				throw new ClassCastException("Cannot cast long to boolean");
 			}
@@ -170,10 +175,10 @@ public interface TypeUtil {
 				throw new ClassCastException("Cannot cast long to int");
 			}
 			if(cls.equals(float.class) || cls.equals(Float.class)) {
-				return (T) (Object) ((Long) val).floatValue();
+				return (T) (Object) v.floatValue();
 			}
 			if(cls.equals(double.class) || cls.equals(Double.class)) {
-				return (T) (Object) ((Long) val).doubleValue();
+				return (T) (Object) v.doubleValue();
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
@@ -229,6 +234,36 @@ public interface TypeUtil {
 			}
 			if(cls.equals(String.class)) {
 				return (T) val;
+			}
+		}
+		if(val instanceof String) {
+			final String s = (String) val;
+			if(cls.equals(boolean.class) || cls.equals(Boolean.class)) {
+				return (T) (Object) Boolean.parseBoolean(s);
+			}
+			if(cls.equals(byte.class) || cls.equals(Byte.class)) {
+				return (T) (Object) Byte.parseByte(s);
+			}
+			if(cls.equals(short.class) || cls.equals(Short.class)) {
+				return (T) (Object) Short.parseShort(s);
+			}
+			if(cls.equals(char.class) || cls.equals(Character.class)) {
+				if(s.length() == 1) {
+					return (T) (Object) s.charAt(0);
+				}
+				throw new ClassCastException("Cannot cast string to char");
+			}
+			if(cls.equals(int.class) || cls.equals(Integer.class)) {
+				return (T) (Object) Integer.parseInt(s);
+			}
+			if(cls.equals(long.class) || cls.equals(Long.class)) {
+				return (T) (Object) Long.parseLong(s);
+			}
+			if(cls.equals(float.class) || cls.equals(Float.class)) {
+				return (T) (Object) Float.parseFloat(s);
+			}
+			if(cls.equals(double.class) || cls.equals(Double.class)) {
+				return (T) (Object) Double.parseDouble(s);
 			}
 		}
 
